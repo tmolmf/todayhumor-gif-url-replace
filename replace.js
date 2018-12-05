b3a7113843a160ec915204789ecf4969cc99a30a
kghx2 <kghx2@winitech.com>
2018-12-05 11:09:28 +0900

    *

--- replace.js
+++ replace.js
@@ -3,7 +3,7 @@
 	videos.forEach(function(video){
 		if(video.children[0].src.includes('__ANIGIF.mp4'))
 			var div = document.createElement('div');
-			var gif_link = video.children[0].src;
+			var gif_link = video.children[0].src; 
 			gif_link = gif_link.replace("__ANIGIF.mp4", "__ANIGIF.gif");
 			div.innerHTML = '<img src="'+gif_link+'" alt="gif image">';
 			video.parentNode.insertBefore(div, video);
