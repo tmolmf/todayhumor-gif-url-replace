0648b974f1582e0561c8bbf19e3cc52ac1679d31
kghx2 <kghx2@winitech.com>
2018-12-05 12:43:47 +0900

    add delay to get videos on reply message

--- replace.js
+++ replace.js
@@ -1,12 +1,16 @@
 (function() {
-	var videos = document.querySelectorAll("video"); 
-	videos.forEach(function(video){
-		if(video.children[0].src.includes('__ANIGIF.mp4'))
-			var div = document.createElement('div');
-			var gif_link = video.children[0].src; 
-			gif_link = gif_link.replace("__ANIGIF.mp4", "__ANIGIF.gif");
-			div.innerHTML = '<img src="'+gif_link+'" alt="gif image">';
-			video.parentNode.insertBefore(div, video);
-			video.parentNode.removeChild(video);
-		})
+	setTimeout(function() {
+		var videos = document.querySelectorAll("video"); 
+			console.log('start');
+		videos.forEach(function(video){
+			console.log(video);
+			if(video.children[0].src.includes('__ANIGIF.mp4'))
+				var div = document.createElement('div');
+				var gif_link = video.children[0].src; 
+				gif_link = gif_link.replace("__ANIGIF.mp4", "__ANIGIF.gif");
+				div.innerHTML = '<img src="'+gif_link+'" alt="gif image">';
+				video.parentNode.insertBefore(div, video);
+				video.parentNode.removeChild(video);
+			})
+	}, 2000)
 })();
