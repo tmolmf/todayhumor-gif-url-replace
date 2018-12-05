7b70dacd38eb3b57ab31d926377df6d304a9b50d
kghx2 <kghx2@winitech.com>
2018-12-05 10:07:08 +0900

    프로젝트 생성

---
+++ inject.js
@@ -0,0 +1,12 @@
+(function() {
+	var videos = document.querySelectorAll("video"); 
+	videos.forEach(function(video){
+		if(video.children[0].src.includes('__ANIGIF.mp4'))
+			var div = document.createElement('div');
+			var gif_link = video.children[0].src;
+			gif_link = gif_link.replace("__ANIGIF.mp4", "__ANIGIF.gif");
+			div.innerHTML = '<img src="'+gif_link+'" alt="gif image">';
+			video.parentNode.insertBefore(div, video);
+			video.parentNode.removeChild(video);
+		})
+})();
